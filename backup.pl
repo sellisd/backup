a6693d0e346f4597120d90de57dfa651d084006e
Diamantis Sellis <sellisd@gmail.com>
2014-12-11 11:23:19 +0100

    does full and incremental backup, bare-bones script, but with potential for improvement

--- backup.pl
+++ backup.pl
@@ -1,16 +1,30 @@
 #!/usr/bin/perl
 use warnings;
 use strict;
+use POSIX qw(strftime);
+
+# using lots of modules (if available) aiming for portability in the near(?) future
+use Module::Load::Conditional qw[can_load check_install requires];
+use File::HomeDir;
+use File::DirList;
 use Getopt::Long;
+#use Sys::Hostname;
 
-
-my $drive; # path to backup drive
-my $type;  # full/incremental
+my $drive =  '/media/dsellis/backupB/'; # path to backup drive
+my $type = 'incremental';  # full/incremental
 my $timeType; # use local time or GMT
-my $hostname; #name of computer
+#my $hostname = hostname(); #name of computer
+my $hostname = `hostname`;
+chomp $hostname;
+my $from = File::HomeDir->my_home.'/';
+my $mostRecentBackup;
+(my $sec,my $min,my $hour,my $mday,my $mon,my $year,my $wday,my $yday,my $isdst) = localtime();
+$year +=1900;
+$mon +=1;
+my $now = sprintf("%04d-%02d-%02d.%02d:%02d:%02d", $year,$mon,$mday,$hour, $min, $sec);
 
-my $time = $localtime(); 
-my @exclude = qw #
+#TODO move the excludes to a configuration file
+my @exclude = qw#
 Trash
 .Thumbnails
 .cache
@@ -21,5 +35,25 @@ Desktop
 Dropbox
 Downloads
 WualaDrive
-#
+#;
 
+my $excludeString = '';
+foreach my $exclude (@exclude){
+  $excludeString .= ' --exclude "'.$exclude.'" ';
+}
+my $deleteString = ' --delete ';
+my $rsyncParamsString = ' -avzh ';
+my $to = $drive.$hostname.'/'.$now.'/';
+my $cmdl = 'rsync ';
+if ($type eq 'full'){
+  $cmdl = $cmdl.$excludeString.$deleteString.$rsyncParamsString.' '.$from.' '.$to;
+}elsif($type eq 'incremental'){
+  my $prevBackupDirs = File::DirList::list($drive.$hostname,'M',1,0);
+#use Data::Dumper;
+#print Dumper $prevBackupDirs->[0][13];die;
+  $cmdl .= $excludeString.$deleteString.$rsyncParamsString.' '.'--link-dest '.$drive.$hostname.'/'.$prevBackupDirs->[0][13].' '.$from.' '.$to;
+}else{
+  die;
+}
+print $cmdl,"\n";
+system "$cmdl > $now.log"; #save the log file in the home directory?
