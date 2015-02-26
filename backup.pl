f1efd091b3ce96b3166a44c022d5f69d00a90cdc
Diamantis Sellis <sellisd@gmail.com>
2015-02-26 13:56:15 +0100

    added command line options destination path is created (if necessary) with File::Path

--- backup.pl
+++ backup.pl
@@ -2,16 +2,40 @@
 use warnings;
 use strict;
 use POSIX qw(strftime);
-
+#TODO make link to the most recent backup for incremental
 # using lots of modules (if available) aiming for portability in the near(?) future
 use Module::Load::Conditional qw[can_load check_install requires];
 use File::HomeDir;
 use File::DirList;
 use Getopt::Long;
+use File::Path qw(make_path);
 #use Sys::Hostname;
 
+#default values
+my $help;
 my $drive =  '/media/dsellis/backupB/'; # path to backup drive
 my $type = 'incremental';  # full/incremental
+my $dry = 0;
+my $usage = <<HERE;
+
+Simple backup using rsync
+usage ./backup.pl [OPTIONS]
+where OPTIONS can be:
+   -to:     path to backup directory (default: /media/dsellis/backubB/)
+   -type:   full/incremental (default: incremental)
+   -dry:    rsync --dry-run
+   -help|?: this help screen
+
+HERE
+    
+die $usage unless (GetOptions('help|?' => \$help,
+			      'to=s'   => \$drive,
+                              'type=s' => \$type,
+                              'dry'    => \$dry
+    ));
+
+die $usage if $help;
+
 my $timeType; # use local time or GMT
 #my $hostname = hostname(); #name of computer
 my $hostname = `hostname`;
@@ -44,16 +68,23 @@ foreach my $exclude (@exclude){
 my $deleteString = ' --delete ';
 my $rsyncParamsString = ' -avzh ';
 my $to = $drive.$hostname.'/'.$now.'/';
+make_path($to) unless -e $to;
 my $cmdl = 'rsync ';
+my $isDryRun;
+if($dry){
+  $isDryRun = ' --dry-run ';
+}else{
+  $isDryRun = ' ';
+}
 if ($type eq 'full'){
-  $cmdl = $cmdl.$excludeString.$deleteString.$rsyncParamsString.' '.$from.' '.$to;
+  $cmdl = $cmdl.$isDryRun.$excludeString.$deleteString.$rsyncParamsString.' '.$from.' '.$to;
 }elsif($type eq 'incremental'){
   my $prevBackupDirs = File::DirList::list($drive.$hostname,'M',1,0);
 #use Data::Dumper;
 #print Dumper $prevBackupDirs->[0][13];die;
-  $cmdl .= $excludeString.$deleteString.$rsyncParamsString.' '.'--link-dest '.$drive.$hostname.'/'.$prevBackupDirs->[0][13].' '.$from.' '.$to;
+  $cmdl .= $isDryRun.$excludeString.$deleteString.$rsyncParamsString.' '.'--link-dest '.$drive.$hostname.'/'.$prevBackupDirs->[0][13].' '.$from.' '.$to;
 }else{
-  die;
+  die "unknown type: ".$type;
 }
 print $cmdl,"\n";
 system "$cmdl > $now.log"; #save the log file in the home directory?
