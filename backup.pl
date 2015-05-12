440f177a77517742b9fbbbc4333d48cc0afeacc8
Diamantis Sellis <sellisd@gmail.com>
2015-05-12 14:29:45 +0200

    add explicit option from and set as default home directory create symbolic link for the most recent backup (to use with incremental mode)

--- backup.pl
+++ backup.pl
@@ -3,6 +3,7 @@ use warnings;
 use strict;
 use POSIX qw(strftime);
 #TODO make link to the most recent backup for incremental
+# add option -info PROGRESS for progress
 # using lots of modules (if available) aiming for portability in the near(?) future
 use Module::Load::Conditional qw[can_load check_install requires];
 use File::HomeDir;
@@ -16,6 +17,8 @@ my $help;
 my $drive =  '/media/dsellis/backupB/'; # path to backup drive
 my $type = 'incremental';  # full/incremental
 my $dry = 0;
+my $from = File::HomeDir->my_home.'/';
+
 my $usage = <<HERE;
 
 Simple backup using rsync
@@ -24,6 +27,7 @@ where OPTIONS can be:
    -to:     path to backup directory (default: /media/dsellis/backubB/)
    -type:   full/incremental (default: incremental)
    -dry:    rsync --dry-run
+   -from:   source (by default home directory)
    -help|?: this help screen
 
 HERE
@@ -31,7 +35,8 @@ HERE
 die $usage unless (GetOptions('help|?' => \$help,
 			      'to=s'   => \$drive,
                               'type=s' => \$type,
-                              'dry'    => \$dry
+                              'dry'    => \$dry,
+			      'from:s'   => \$from
     ));
 
 die $usage if $help;
@@ -40,8 +45,6 @@ my $timeType; # use local time or GMT
 #my $hostname = hostname(); #name of computer
 my $hostname = `hostname`;
 chomp $hostname;
-my $from = File::HomeDir->my_home.'/';
-my $mostRecentBackup;
 (my $sec,my $min,my $hour,my $mday,my $mon,my $year,my $wday,my $yday,my $isdst) = localtime();
 $year +=1900;
 $mon +=1;
@@ -59,6 +62,7 @@ Desktop
 Dropbox
 Downloads
 WualaDrive
+data/overdominance
 #;
 
 my $excludeString = '';
@@ -69,6 +73,25 @@ my $deleteString = ' --delete ';
 my $rsyncParamsString = ' -avzh ';
 my $to = $drive.$hostname.'/'.$now.'/';
 make_path($to) unless -e $to;
+
+my $mostRecentBackup = $drive.$hostname.'/mostRecent';
+
+if ($dry){
+# do nothing
+}else{
+    if(-e $mostRecentBackup){ # if link is present
+        #use it and replace it with the new one
+        system 'ln -s '.$to.' '.$mostRecentBackup;
+    }else{ # if not present
+        if($type eq 'full'){
+            # create new link
+            system 'ln -s '.$to.' '.$mostRecentBackup;
+        }elsif($type eq 'incremental'){
+	    die "Cannot do incremental backup without previous ones!";
+        }
+    }
+}
+#die $to;
 my $cmdl = 'rsync ';
 my $isDryRun;
 if($dry){
@@ -79,12 +102,15 @@ if($dry){
 if ($type eq 'full'){
   $cmdl = $cmdl.$isDryRun.$excludeString.$deleteString.$rsyncParamsString.' '.$from.' '.$to;
 }elsif($type eq 'incremental'){
-  my $prevBackupDirs = File::DirList::list($drive.$hostname,'M',1,0);
+#  my $prevBackupDirs = $mostRecentBackup;
 #use Data::Dumper;
-#print Dumper $prevBackupDirs->[0][13];die;
-  $cmdl .= $isDryRun.$excludeString.$deleteString.$rsyncParamsString.' '.'--link-dest '.$drive.$hostname.'/'.$prevBackupDirs->[0][13].' '.$from.' '.$to;
+#print Dumper $prevBackupDirs;die;
+  $cmdl .= $isDryRun.$excludeString.$deleteString.$rsyncParamsString.' '.'--link-dest '.$mostRecentBackup.' '.$from.' '.$to;
 }else{
   die "unknown type: ".$type;
 }
 print $cmdl,"\n";
 system "$cmdl > $now.log"; #save the log file in the home directory?
+
+# check for any space issues
+print `df -h`;
